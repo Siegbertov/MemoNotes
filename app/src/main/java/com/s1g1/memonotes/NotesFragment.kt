a0c907fa0000f2e1d7a3b9c35fa4351b938b7e2a
Sieg Bertov <siegbertov@gmail.com>
2026-04-03 15:39:49 +0300

    ui: create actual navigation btwn Fragments

---
+++ app/src/main/java/com/s1g1/memonotes/NotesFragment.kt
@@ -0,0 +1,5 @@
+package com.s1g1.memonotes
+
+import androidx.fragment.app.Fragment
+
+class NotesFragment : Fragment(R.layout.fragment_notes)
---
+++ app/src/main/java/com/s1g1/memonotes/SettingsFragment.kt
@@ -0,0 +1,5 @@
+package com.s1g1.memonotes
+
+import androidx.fragment.app.Fragment
+
+class SettingsFragment : Fragment(R.layout.fragment_settings)
