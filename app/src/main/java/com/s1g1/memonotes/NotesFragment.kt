68637cc979758b235d83b9a8c9b9a4f346593a70
Sieg Bertov <siegbertov@gmail.com>
2026-04-03 19:50:41 +0300

    ui: create dummy FAB on NotesFragment

--- app/src/main/java/com/s1g1/memonotes/NotesFragment.kt
+++ app/src/main/java/com/s1g1/memonotes/NotesFragment.kt
@@ -1,5 +1,26 @@
 package com.s1g1.memonotes
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.s1g1.memonotes.databinding.FragmentNotesBinding
 
-class NotesFragment : Fragment(R.layout.fragment_notes)
+class NotesFragment : Fragment(R.layout.fragment_notes){
+    private var _binding: FragmentNotesBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentNotesBinding.bind(view)
+
+        binding.fabAddNote.setOnClickListener {
+            Toast.makeText(requireContext(), "CLICKED ON ADD FAB", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
