3b9036014213f79fd5af62baaf4a49c30a133f8f
Sieg Bertov <siegbertov@gmail.com>
2026-04-04 14:45:15 +0300

    feat: implement NoteViewModel and Factory with manual DI

---
+++ app/src/main/java/com/s1g1/memonotes/viewmodel/NoteViewModel.kt
@@ -0,0 +1,47 @@
+package com.s1g1.memonotes.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.s1g1.memonotes.database.NoteEntity
+import com.s1g1.memonotes.database.NoteRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class NoteViewModel(
+    private val repository: NoteRepository
+): ViewModel() {
+
+    val allNotes = repository.allNotes.asLiveData()
+
+    suspend fun getNoteById(id: Int) : NoteEntity{
+        return repository.getNoteById(id=id)
+    }
+
+    fun upsert(noteEntity: NoteEntity){
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.upsert(noteEntity=noteEntity)
+        }
+    }
+
+    fun delete(noteEntity: NoteEntity){
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.delete(noteEntity=noteEntity)
+        }
+    }
+
+}
+
+
+class NoteViewModelFactory(private val repository: NoteRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(NoteViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return NoteViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
